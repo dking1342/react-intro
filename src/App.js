2adc854c3d776a3a0599ce49928835d4aa32b4b3
dking1342 <leanmachine42@yahoo.com>
2020-12-31 01:28:25 -0600

    init commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,137 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+// custom components
+import List from './components/List';
+import Alert from './components/Alert';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list');
+  if(list){
+    return JSON.parse(localStorage.getItem('list'))
+  } else{
+    return []
+  }
+}
+
+const App = () => {
+  // state
+  const [name,setName]=useState('');
+  const [list,setList]=useState(getLocalStorage());
+  const [isEditing,setIsEditing]=useState(false);
+  const [editID, setEditID]=useState(null);
+  const [alert,setAlert]=useState({
+    show:false,
+    msg:'',
+    type:''
+  });
+
+  // helper functions 
+
+  // show alert function
+  const showAlert = (show=false,type="",msg="")=>{
+    setAlert({show, type, msg})
+  }
+
+  //  CRUD FUNCTIONS
+  // clears the list
+  const clearList = () => {
+    showAlert(true,'danger','emptied the list');
+    setList([]);
+  }
+
+  // removes single item
+  const removeItem = (id) => {
+    showAlert(true,'danger','item removed')
+    setList(list.filter(item=> item.id !== id));
+  }
+
+  // edits the item
+  const editItem = (id) => {
+    const specificItem = list.find((item)=> item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title)
+  }
+
+  // hooks
+  useEffect(()=>{
+    localStorage.setItem('list',JSON.stringify(list))
+  },[list])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!name){
+      // display alert
+      showAlert(true,'danger','Please enter value');
+    } 
+    else if(name && isEditing){
+
+      // setting list state
+      setList(list.map((item)=>{
+        if(item.id === editID){
+          return { ...item, title:name }
+        }
+        return item;
+      }))
+
+      // set other state
+      setIsEditing(false);
+      setEditID(null);
+      setName('');
+
+      // display alert
+      showAlert(true,'success','item has been edited');
+    } 
+    else {
+      // show alert
+      showAlert(true,'success','item added to the list');
+
+      // get new state
+      const newItem={
+        id:new Date().getTime().toString(),
+        title:name
+      };
+
+      // set state
+      setList([...list, newItem]);
+      setName('');
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <form onSubmit={ handleSubmit } className="grocery-form">
+
+        {alert.show && <Alert { ...alert } removeAlert={ showAlert } list={ list }/>}
+
+        <h3>grocery bud</h3>
+        <div className="form-control">
+          <input 
+            type="text" 
+            className="grocery" 
+            placeholder="e.g. eggs"
+            value={ name }
+            onChange={ (e)=> setName(e.target.value) }
+          />
+
+          <button className="submit-btn" type="submit">
+            { isEditing ? 'edit' : 'submit' }
+          </button>
+        </div>
+      </form>
+
+      {list.length > 0 && (
+      <div className="grocery-container">
+          <List 
+            items={ list }
+            removeItem={ removeItem }
+            editItem={ editItem }
+          />
+          <button onClick={ clearList } className="clear-btn">clear items</button>
+        </div>
+      )}
+    </section>
   );
 }
 
---
+++ src/components/List.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { FaEdit, FaTrash } from 'react-icons/fa';
+
+const List = ({ items, removeItem, editItem }) => {
+    return (
+        <div className="grocery-list">
+          {
+              items.map(item=>(
+                <article className="grocery-item" key={ item.id }>
+                    <p className="title">{ item.title }</p>
+                    <div className="btn-container">
+                        <button className="edit-btn" type="button" onClick={ ()=>editItem(item.id) }>
+                            <FaEdit />
+                        </button>
+                        <button className="delete-btn" onClick={ ()=> removeItem(item.id)}>
+                            <FaTrash />
+                        </button>
+                    </div>
+                </article>
+              ))
+          }
+        </div>
+    )
+}
+
+export default List
